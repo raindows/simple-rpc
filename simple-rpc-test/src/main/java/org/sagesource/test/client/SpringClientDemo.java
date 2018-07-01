0fc518d501e3796e4c042493d5dba2f8dccd8c13
sage.xue <job.xueqi@outlook.com>
2018-07-01 18:40:47 +0800

    客户端新增基本的 Spring 支持

---
+++ simple-rpc-test/src/main/java/org/sagesource/test/client/SpringClientDemo.java
@@ -0,0 +1,32 @@
+package org.sagesource.test.client;
+
+import org.apache.thrift.TException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sagesource.test.api.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * <p></p>
+ * <pre>
+ *     author      XueQi
+ *     date        2018/7/1
+ *     email       job.xueqi@outlook.com
+ * </pre>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath*:spring-client.xml"})
+public class SpringClientDemo {
+
+	@Autowired
+	@Qualifier("helloWorldService")
+	private HelloWorldService.Iface helloWorldService;
+
+	@Test
+	public void test() throws TException {
+		System.out.println(helloWorldService.sayHello("`111111"));
+	}
+}
